297a325d12d0ace4090d393655fa8923fef37fff
tobakken <tobakken@stud.ntnu.no>
2022-01-28 11:40:19 +0100

    Completed calculator assignment

--- Oving2/calculator/src/components/Calculator.vue
+++ Oving2/calculator/src/components/Calculator.vue
@@ -1,14 +1,28 @@
 <template>
-  <div class="input">
-    <p>{{calculatorValue}}</p>
+  <div class="calc-body">
+    <div class="input">
+      {{ calculatorValue || 0 }}
+    </div>
+    <div>
+      <div class="grid-container">
+        <button
+          class="btn"
+          v-bind:class="{ 'event-btn': calculatorEvents.includes(n) }"
+          v-for="n in calculatorBtn"
+          :key="n"
+          @click="add(n)"
+        >
+          {{ n }}
+        </button>
+      </div>
+    </div>
   </div>
-  <div>
-    <button v-for="n in calculatorNmb" :key="n"
-            @click="add(n)" >{{n}}</button>
+  <div class="log">
+    <p>Previous input:</p>
+    <div v-for="ev in calculatorLog" :key="ev">
+      {{ ev }}
+    </div>
   </div>
-  <button v-for="n in calculatorEvents" :key="n"
-          @click="event(n)"> {{n}}</button>
-  <div v-for="ev in calculatorLog" :key="ev"> {{ev}}</div>
 </template>
 
 <script>
@@ -16,61 +30,111 @@ export default {
   name: "Calculator",
   data() {
     return {
-      calculatorValue: '',
-      prevCalcValue: '',
+      calculatorValue: "",
+      prevCalcValue: "",
       operator: null,
-      calculatorNmb: [
-          '1',
-          '2',
-          '3',
-          '4',
-          '5',
-          '6',
-          '7',
-          '8',
-          '9',
-          '0'
+      calculatorBtn: [
+        "7",
+        "8",
+        "9",
+        "/",
+        "4",
+        "5",
+        "6",
+        "*",
+        "1",
+        "2",
+        "3",
+        "-",
+        "0",
+        ".",
+        "+",
+        "=",
+        "AC",
       ],
-      calculatorEvents: [
-          '+',
-          '-',
-          '*',
-          '/',
-          '=',
-          'AC'
-      ],
-      calculatorLog: []
-    }
+      calculatorEvents: ["+", "-", "*", "/", "=", "AC"],
+      calculatorLog: [],
+    };
   },
 
   methods: {
-    add(n){
-      this.calculatorValue += n
+    add(n) {
+      if (this.calculatorEvents.includes(n)) this.event(n);
+      else this.calculatorValue += n;
     },
     event(n) {
-     if (n === 'AC'){
-       this.calculatorValue = ''
-     }
-     if (['/', '+', '-', '*'].includes(n)) {
-       this.operator = n;
-       this.prevCalcValue = this.calculatorValue;
-       this.calculatorValue = ''
-     }
-      if (n === '=') {
-        var result = this.prevCalcValue + this.operator + this.calculatorValue;
+      if (n === "AC") {
+        this.calculatorValue = "";
+      }
+      if (["/", "+", "-", "*"].includes(n)) {
+        if (this.operator === null){
+          this.operator = n;
+          this.prevCalcValue = this.calculatorValue;
+          this.calculatorValue = "";
+        } else this.operator = n;
+      }
+      if (n === "=") {
+        const result =
+            this.prevCalcValue + this.operator + this.calculatorValue;
         this.calculatorValue = eval(result);
-        this.calculatorLog.push(result + '=' + this.calculatorValue);
-        this.operator = null
-     }
-    }
-  }
-}
+        this.calculatorLog.push(result + "=" + this.calculatorValue);
+        this.operator = null;
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
-.input{
-  border-style: solid;
-  width: 50%;
+.calc-body {
+  background-color: #aaaaaa;
+  width: 25%;
   margin: auto;
+  padding: 10px;
+}
+
+.input {
+  font-size: 30px;
+  background-color: #f5f5f5;
+  border: 1px solid;
+  border-radius: 15px;
+  width: 75%;
+  margin: auto;
+}
+
+.grid-container {
+  margin: auto;
+  width: 260px;
+}
+
+.log {
+  border: 2px #aaaaaa dashed;
+  border-radius: 10px;
+  padding: 10px;
+  width: 30%;
+  margin: 20px auto auto;
+}
+
+.log p {
+  margin-top: 0px;
+}
+
+.btn {
+  font-size: 28px;
+  width: 50px;
+  margin-left: 10px;
+  margin-top: 10px;
+  border: 2px solid black;
+  color: #ff8c00;
+  background-color: #2c3e50;
+  transition-duration: 0.2s;
+}
+
+.btn:hover {
+  transform: scale(1.05);
+}
+
+.event-btn {
+  color: #ff0000;
 }
 </style>
--- Oving2/calculator/src/views/Home.vue
+++ Oving2/calculator/src/views/Home.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+  <div class="heading">Calculator</div>
     <Calculator />
-  </div>
 </template>
 
 <script>
@@ -16,3 +14,12 @@ export default {
   },
 };
 </script>
+
+<style>
+.heading {
+  font-size: 36px;
+  font-weight: bold;
+  margin-top: 100px;
+}
+
+</style>
