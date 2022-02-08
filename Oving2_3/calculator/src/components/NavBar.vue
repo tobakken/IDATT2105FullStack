aee6ead00a96a340108b202cdbf274bf232840d6
tobakken <tobakken@stud.ntnu.no>
2022-02-08 11:14:27 +0100

    Changed name of project and added components for forms

---
+++ Oving2_3/calculator/src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="nav">
+    <router-link :to="{name: 'Calculator'}">Calculator</router-link>
+    <router-link :to="{name: 'Contact'}">Contact</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
+
+<style scoped>
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+</style>
--- Oving2_3/calculator/src/router/index.js
+++ Oving2_3/calculator/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Contact from "../views/Contact";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: Contact
   }
 ];
 
